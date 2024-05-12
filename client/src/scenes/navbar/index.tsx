7cf40e0c030ca60bd344cc3540c5a9344d5e7618
WilsonCruzz <wilson860731@gmail.com>
2024-05-12 14:17:25 -0400

    fix

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -8,7 +8,7 @@ type Props = {};
 
 const Navbar = (props: Props) => {
     const {palette} = useTheme();
-    const {selected, setSelected} = useState("dashboard");
+    const [selected, setSelected] = useState("dashboard");
   return (
     <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
         {/* LEFT SIDE */}
